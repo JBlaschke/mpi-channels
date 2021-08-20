4d9d0baec65d1820ba3d8a1b46a18912afd341f5
Johannes Blaschke <jpblaschke@lbl.gov>
2021-08-19 21:51:12 -0700

    build better test

--- consumer.py
+++ consumer.py
@@ -7,7 +7,7 @@ from   mpi4py   import MPI
 from   logging  import DEBUG, basicConfig
 from   argparse import ArgumentParser
 
-from producer import Producer
+from producer import Producer, make_win
 
 
 comm = MPI.COMM_WORLD
@@ -24,23 +24,53 @@ message_size = 10
 
 producer = Producer(buff_size, message_size)
 
+
+p_sum_win = make_win(
+    dtype = MPI.DOUBLE,
+    n_buf = 1,
+    comm  = comm,
+    host  = 0
+)
+
+
+
 if args.logging:
     producer.buf.log.setLevel(DEBUG)
     basicConfig(filename=f"{rank:0>3}.log", level=DEBUG)
 
-data = np.random.rand(data_size, 6)
-# producer.fill(data)
-producer.claim(len(data))
-for elt in data:
-    producer.put(elt)
+if rank == 0:
+    data = np.random.rand(data_size, 6)
+    # producer.fill(data)
+    producer.claim(len(data))
+    p_sum = 0.
+    for elt in data:
+        producer.put(elt)
+        p_sum += np.sum(elt)
+    print(f"{rank=} {p_sum=}")
 
-# print(f"{rank=} {data=}")
 
-res = 0
-for i in range(data_size):
-    p = producer.take(1)
-    if p is not None:
-        print(f"{rank=}, {i=}, {p=}")
-        res += 1
+if rank > 0:
+    res = 0
+    p_sum = 0.
+    for i in range(data_size):
+        p = producer.take(1)
+        if p is not None:
+            print(f"{rank=}, {i=}, {p=}")
+            p_sum += np.sum(p)
+            res += 1
 
-print(f"{rank=} {res=}")
+    print(f"{rank=} {res=}")
+    p_sum_win.Lock(rank=0)
+    buf = np.empty(1, dtype=np.float64)
+    incr = np.empty(1, dtype=np.float64)
+    incr[0] = p_sum
+    p_sum_win.Get_accumulate(incr, buf, target_rank=0)
+    p_sum_win.Unlock(rank=0)
+
+comm.Barrier()
+
+if rank == 0:
+    p_sum_win.Lock(rank=0)
+    p_sum_buf = np.frombuffer(p_sum_win, dtype=np.float64)
+    p_sum_win.Unlock(rank=0)
+    print(f"{rank=} {p_sum_buf=} : {p_sum - p_sum_buf[0]}")
